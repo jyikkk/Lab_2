a314867ec20c0fc7d131d110b5977e4c8223dacb
jiayik <ljyik.24@ichat.sp.edu.sg>
2025-04-28 10:58:10 +0800

    added bmi calculator

---
+++ bmi.py
@@ -0,0 +1,13 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight / (height * height)
+    print ("Bmi = "+ str(bmi))
+    if bmi < 18.5:
+        print("you are underweighttttt")
+    if bmi > 18.5 and bmi < 25:
+        print("you are normalweightttt YAH")
+    if bmi > 25:
+        print("you are overweighttt ?????")
+
+calculate_bmi(1.76, 58.5)
